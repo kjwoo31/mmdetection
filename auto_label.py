1610aeb62c231f4cc7d190b3d1257c54557e7ff7
RobotisDongun <kimjw@robotis.com>
2025-01-13 14:58:05 +0900

    add custom_entities for grounding DINO visualization

--- auto_label.py
+++ auto_label.py
@@ -8,6 +8,7 @@ model_name = 'CODETR'
 score_threshold = 0.5
 batch_size = 1
 text_prompt = None
+custom_entities = False
 draw_pred = True  # Whether to draw predicted bounding boxes.
 no_save_pred = False  # Whether to force not to save prediction vis results
 return_labelme = True  # Whether to return results as labelme format
@@ -15,11 +16,12 @@ return_labelme = True  # Whether to return results as labelme format
 # Model
 if model_name == 'CODETR':
     model_path = 'projects/CO-DETR/configs/codino/co_dino_5scale_swin_l_16xb1_16e_ev_state.py'
-    weight_path = '/mnt/DL_TOOLS/mmdetection/work_dirs/co_dino_5scale_swin_l_16xb1_16e_ev_state/best_coco_bbox_mAP_epoch_26.pth'
+    weight_path = '/mnt/DL_TOOLS/mmdetection/work_dirs/co_dino_5scale_swin_l_16xb1_16e_ev_state/best_coco_bbox_mAP_epoch_32.pth'
 elif model_name == 'GroundingDINO': # Need to change text prompt to small letter, alphabet
     model_path = 'configs/mm_grounding_dino/coco/grounding_dino_swin-t_finetune_16xb4_1x_ev_state.py'
     weight_path = '/mnt/DL_TOOLS/mmdetection/work_dirs/grounding_dino_swin-t_finetune_16xb4_1x_ev_state/epoch_16.pth'
     text_prompt = 'Door_open . Door_close . Door_moving . Door_sticker . Indicator . Hall_LED . Hall_LED_on . Hall_LED_off . up . down . unknown . 9 . 8 . 7 . 6 . 5 . 4 . 3 . 2 . 1 . 0 . L . B . P . G . R . F . N . M .'
+    custom_entities = True
 
 # Initialize the DetInferencer
 inferencer = DetInferencer(
@@ -36,4 +38,5 @@ inferencer(
     pred_score_thr=0.5,
     no_save_pred=no_save_pred,
     return_labelme=return_labelme,
-    texts=text_prompt)
+    texts=text_prompt,
+    custom_entities=custom_entities)
